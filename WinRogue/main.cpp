6c39ef9757f0757d1de134d10529b054773aef97
mikeyk730 <mikeyk730@gmail.com>
2016-10-05 18:19:09 -0400

    code to generate letter tilemap

--- WinRogue/main.cpp
+++ WinRogue/main.cpp
@@ -4,7 +4,37 @@
 #include "SDL.h"
 #include "SDL_ttf.h"
 #include "res_path.h"
-#include "..\rogue.h"
+
+#define PASSAGE   (0xb1)
+#define DOOR      (0xce)
+#define FLOOR     (0xfa)
+#define PLAYER    (0x01)
+#define TRAP      (0x04)
+#define STAIRS    (0xf0)
+#define GOLD      (0x0f)
+#define POTION    (0xad)
+#define SCROLL    (0x0d)
+#define MAGIC     '$'
+#define BMAGIC    '+'
+#define FOOD      (0x05)
+#define STICK     (0xe7)
+#define ARMOR     (0x08)
+#define AMULET    (0x0c)
+#define RING      (0x09)
+#define WEAPON    (0x18)
+#define MISSILE   '*'
+#define VWALL   (0xba)
+#define HWALL   (0xcd)
+#define ULWALL  (0xc9)
+#define URWALL  (0xbb)
+#define LLWALL  (0xc8)
+#define LRWALL  (0xbc)
+
+struct Coord
+{
+    int x;
+    int y;
+};
 
 
 const int H_PIXELS_PER_CHAR = 8;
@@ -29,17 +59,22 @@ namespace SDL
 
     namespace Colors
     {
-        SDL_Color black()    { return {   0,   0,   0, 255 }; }
-        SDL_Color white()    { return { 255, 255, 255, 255 }; }
-        SDL_Color grey()     { return { 170, 170, 170, 255 }; }
-        SDL_Color red()      { return { 170,   0,   0, 255 }; }
-        SDL_Color green()    { return {   0, 170,   0, 255 }; }
-        SDL_Color l_green()  { return {  85,  255, 85, 255 }; }
-        SDL_Color blue()     { return {   0,   0, 170, 255 }; }
-        SDL_Color l_blue()   { return {  85,  85, 255, 255 }; }
-        SDL_Color magenta()  { return { 170,   0, 170, 255 }; }
-        SDL_Color yellow()   { return { 255, 255,  25, 255 }; }
-        SDL_Color brown()    { return { 170,  85,   0, 255 }; }
+        SDL_Color black()    { return {   0,   0,   0, 255 }; } //from dosbox
+        SDL_Color white()    { return { 255, 255, 255, 255 }; } //from dosbox
+        SDL_Color grey()     { return { 170, 170, 170, 255 }; } //from dosbox
+        SDL_Color d_grey()   { return {  65,  65,  65, 255 }; }
+        SDL_Color l_grey()   { return { 205, 205, 205, 255 }; }
+        SDL_Color red()      { return { 170,   0,   0, 255 }; } //from dosbox
+        SDL_Color l_red()    { return { 255,  85,  85, 255 }; }
+        SDL_Color green()    { return {   0, 170,   0, 255 }; } //from dosbox
+        SDL_Color l_green()  { return {  85,  255, 85, 255 }; } //from dosbox
+        SDL_Color blue()     { return {   0,   0, 170, 255 }; } //from dosbox
+        SDL_Color l_blue()   { return {  85,  85, 255, 255 }; } //from dosbox
+        SDL_Color cyan()     { return {   0, 170, 170, 255 }; }
+        SDL_Color magenta()  { return { 170,   0, 170, 255 }; } //from dosbox
+        SDL_Color l_magenta(){ return { 255,  25, 255, 255 }; } 
+        SDL_Color yellow()   { return { 255, 255,  25, 255 }; } //from dosbox
+        SDL_Color brown()    { return { 170,  85,   0, 255 }; } //from dosbox
     }
 }
 
@@ -233,7 +268,7 @@ SDL_Rect get_rect(int i)
     return r;
 }
 
-SDL::Scoped::Texture texture_for_byte(byte chr, bool in_room, SDL_Surface* surface, SDL_Renderer* renderer)
+SDL::Scoped::Texture texture_for_byte(int chr, bool in_room, SDL_Surface* surface, SDL_Renderer* renderer)
 {
     SDL_Rect r = get_rect(chr);
     using namespace SDL::Colors;
@@ -293,10 +328,29 @@ void add(int c, int i, SDL_Surface* tiles, SDL_Renderer* renderer) {
     render_texture_at(t2.release(), renderer, { 8 * i, 16 }, nullptr);
 }
 
+void output_tilemap(SDL_Surface* tiles, SDL_Renderer* renderer)
+{
+    SDL_RenderClear(renderer);
+    int n = 0;
+    std::vector<int> v = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+        PLAYER, ULWALL, URWALL, LLWALL, LRWALL, HWALL, VWALL, FLOOR, FLOOR, DOOR, STAIRS, TRAP, AMULET, FOOD, GOLD, POTION, RING,
+        SCROLL, STICK, WEAPON, WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,ARMOR, ARMOR, ARMOR, ARMOR, ARMOR,
+        ARMOR, ARMOR, ARMOR, MAGIC, BMAGIC, '\\', '/', '-', '|', '\\', '/', '-', '|', '\\', '/', '-', '|',MISSILE };
+    for (auto i = v.begin(); i != v.end(); ++i)
+    {
+        add(*i, n++, tiles, renderer);
+    }
+    SDL_RenderPresent(renderer);
+}
+
 
 int main(int argc, char** argv)
 {
     using namespace SDL::Scoped;
+
+    int a;
+    std::cin >> a;
+    std::cout << a;
 
     try {
 
@@ -306,7 +360,7 @@ int main(int argc, char** argv)
         //if (TTF_Init() != 0)
         //    throw_error("TTF_Init");
 
-        Window win(SDL_CreateWindow("Rogue", 100, 50, 8*78, 32, SDL_WINDOW_SHOWN), SDL_DestroyWindow);
+        Window win(SDL_CreateWindow("Rogue", 100, 50, 8*128, WINDOW_Y, SDL_WINDOW_SHOWN), SDL_DestroyWindow);
         if (win == nullptr)
             throw_error("SDL_CreateWindow");
 
@@ -317,17 +371,28 @@ int main(int argc, char** argv)
         Surface tiles(load_bmp(getResourcePath("") + "dos24.bmp"));
 
         using namespace SDL::Colors;
-        Texture standard(create_texture(tiles.get(), renderer.get()));
-        Texture v1(painted_texture(tiles.get(), 0, grey(), black(), renderer.get()));
-        Texture v2(painted_texture(tiles.get(), 0, l_blue(), black(), renderer.get()));
-        Texture v3(painted_texture(tiles.get(), 0, red(), black(), renderer.get()));
-        Texture v4(painted_texture(tiles.get(), 0, red(), grey(), renderer.get()));
-        Texture v5(painted_texture(tiles.get(), 0, black(), grey(), renderer.get()));
-        Texture v6(painted_texture(tiles.get(), 0, black(), green(), renderer.get()));
-        Texture v7(painted_texture(tiles.get(), 0, blue(), grey(), renderer.get()));
-        Texture v8(painted_texture(tiles.get(), 0, green(), black(), renderer.get()));
+        std::vector<Texture> textures;
+        textures.push_back(std::move(create_texture(tiles.get(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, green(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, cyan(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, red(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, magenta(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, brown(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, d_grey(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, l_blue(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, l_grey(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, l_red(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, l_magenta(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, yellow(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, grey(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, blue(), black(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, black(), grey(), renderer.get())));
+        textures.push_back(std::move(painted_texture(tiles.get(), 0, white(), black(), renderer.get())));
+        //textures.push_back(std::move(painted_texture(tiles.get(), 0, black(), grey(), renderer.get())));
 
         SDL_StartTextInput();
+
+        output_tilemap(tiles.get(), renderer.get());
 
         char key = 1;
         //Our event structure
@@ -350,31 +415,15 @@ int main(int argc, char** argv)
 
             SDL_RenderClear(renderer.get());
 
-            //render_texture_at(standard.get(), renderer.get(), { 300,16 }, 0);
-            /*
-            for (int i = 0; i < 256; ++i) {
-            SDL_Rect r = get_rect(i);
-            render_texture_at(v1.get(), renderer.get(), { 8 * i, 0 }, r);
-            render_texture_at(v2.get(), renderer.get(), { 8 * i, 16 }, r);
-            render_texture_at(v3.get(), renderer.get(), { 8 * i, 16 * 2 }, r);
-            render_texture_at(v4.get(), renderer.get(), { 8 * i, 16 * 3 }, r);
-            render_texture_at(v5.get(), renderer.get(), { 8 * i, 16 * 4 }, r);
-            render_texture_at(v6.get(), renderer.get(), { 8 * i, 16 * 5 }, r);
-            render_texture_at(v7.get(), renderer.get(), { 8 * i, 16 * 6 }, r);
-            render_texture_at(v8.get(), renderer.get(), { 8 * i, 16 * 7 }, r);
-            }*/
-
-            int n = 0;
-            std::vector<int> v = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                PLAYER, ULWALL, URWALL, LLWALL, LRWALL, HWALL, VWALL, FLOOR, FLOOR, DOOR, STAIRS, TRAP, AMULET, FOOD, GOLD, POTION, RING,
-                SCROLL, STICK, WEAPON, WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,WEAPON,ARMOR, ARMOR, ARMOR, ARMOR, ARMOR,
-                ARMOR, ARMOR, ARMOR, '$', '+', '\\', '/', '-', '|', '\\', '/', '-', '|', '\\', '/', '-', '|','*'};
-            for (auto i = v.begin(); i != v.end(); ++i)
-            {
-                add(*i, n++, tiles.get(), renderer.get());
+            //render_texture_at(standard.get(), renderer.get(), { 16, 48 }, 0);
+            for (int i = 0x20, n = 0; i < 128; ++i, ++n) {
+                SDL_Rect r = get_rect(i);
+                int k = 0;
+                for (auto it = textures.begin(); it != textures.end(); ++it)
+                    render_texture_at((*it).get(), renderer.get(), { 8*n, 16*k++}, r);
             }
 
-
+            
             SDL_RenderPresent(renderer.get());
         }
 
