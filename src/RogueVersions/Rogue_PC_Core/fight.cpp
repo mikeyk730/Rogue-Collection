dd2046136df1daef3b50c765c03beea5f8cac736
unknown <mikeyk730@gmail.com>
2016-12-02 01:57:24 -0500

    don't clear tile if tile_beneath isn't set

--- src/RogueVersions/Rogue_PC_Core/fight.cpp
+++ src/RogueVersions/Rogue_PC_Core/fight.cpp
@@ -178,7 +178,7 @@ void remove_monster(Monster* monster, bool waskill)
     if (game->level().get_tile(monster_pos) == PASSAGE)
         game->screen().standout();
     //mdk:bugfix: don't reveal out of sight tile (unless stairs)
-    if (!game->hero().can_see(monster_pos) && monster->tile_beneath() != STAIRS)
+    if (!game->hero().can_see(monster_pos) && monster->has_tile_beneath() && monster->tile_beneath() != STAIRS)
     //if (monster->tile_beneath() == FLOOR && !game->hero().can_see(monster_pos))
         game->screen().add_tile(monster_pos, ' ');
     else if (monster->has_tile_beneath())
