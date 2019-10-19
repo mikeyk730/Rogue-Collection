a28e51c64e53fb3876d24cd9f5db8b917e630586
Mike Kaminski <mikeyk730@gmail.com>
2019-10-19 00:37:49 -0700

    Move release staging to bin dir

--- src/RogueCollectionQml/make_release.bat
+++ src/RogueCollectionQml/make_release.bat
@@ -1,30 +1,27 @@
-xcopy ..\..\res\tilemap_v1.bmp .\staging\res\
-xcopy ..\..\res\tilemap_v2.bmp .\staging\res\
-xcopy ..\..\res\tilemap_v3.bmp .\staging\res\
-xcopy ..\..\res\tilemap_v4.bmp .\staging\res\
-xcopy ..\..\res\tilemap_text.bmp .\staging\res\
-xcopy ..\..\res\boxy.bmp .\staging\res\
-xcopy ..\..\res\fonts\Px437_IBM_VGA8.ttf .\staging\res\fonts\
-xcopy ..\..\res\fonts\*.txt .\staging\res\fonts\
-xcopy ..\..\res\sounds\*.wav .\staging\res\sounds\
+xcopy ..\..\res\tilemap_v1.bmp ..\..\bin\staging\res\
+xcopy ..\..\res\tilemap_v2.bmp ..\..\bin\staging\res\
+xcopy ..\..\res\tilemap_v3.bmp ..\..\bin\staging\res\
+xcopy ..\..\res\tilemap_v4.bmp ..\..\bin\staging\res\
+xcopy ..\..\res\tilemap_text.bmp ..\..\bin\staging\res\
+xcopy ..\..\res\boxy.bmp ..\..\bin\staging\res\
+xcopy ..\..\res\fonts\Px437_IBM_VGA8.ttf ..\..\bin\staging\res\fonts\
+xcopy ..\..\res\fonts\*.txt ..\..\bin\staging\res\fonts\
+xcopy ..\..\res\sounds\*.wav ..\..\bin\staging\res\sounds\
 
-xcopy ..\..\data\scrolls.dat .\staging\data\
-xcopy ..\..\data\potions.dat .\staging\data\
-xcopy ..\..\data\sticks.dat .\staging\data\
-xcopy ..\..\data\rings.dat .\staging\data\
+xcopy ..\..\data\scrolls.dat ..\..\bin\staging\data\
+xcopy ..\..\data\potions.dat ..\..\bin\staging\data\
+xcopy ..\..\data\sticks.dat ..\..\bin\staging\data\
+xcopy ..\..\data\rings.dat ..\..\bin\staging\data\
 
-xcopy ..\..\src\RogueCollectionQml\gpl-3.0.txt .\staging\license\
-copy ..\..\src\RogueVersions\Rogue_5_4_2\LICENSE.TXT  .\staging\license\unix-rogue.txt
+xcopy ..\..\src\RogueCollectionQml\gpl-3.0.txt ..\..\bin\staging\license\
+copy ..\..\src\RogueVersions\Rogue_5_4_2\LICENSE.TXT  ..\..\bin\staging\license\unix-rogue.txt
 
-xcopy ..\..\src\build-RogueCollection-Static_32bit-Release\RogueCollection.exe .\staging\
-xcopy ..\..\src\build-RogueCollection-Static_32bit-Release\RoguePlugin\qmldir .\staging\RoguePlugin\
-xcopy ..\..\src\build-RetroRogueCollection-Static_32bit-Release\RetroRogueCollection.exe .\staging\
-xcopy ..\..\src\build-RetroRogueCollection-Static_32bit-Release\RoguePlugin\qmldir .\staging\RoguePlugin\
-xcopy ..\..\bin\Win32\Release\Rogue_*.dll .\staging\
-xcopy ..\..\bin\Win32\Release\Rogomatic*.dll .\staging\
-xcopy ..\..\rogue.opt .\staging\
-xcopy ..\..\docs\readme.md .\staging\
-
-cd staging
-mkdir rlog
-cd ..
+xcopy ..\..\bin\build-RogueCollection-Static_32bit-Release\RogueCollection.exe ..\..\bin\staging\
+xcopy ..\..\bin\build-RogueCollection-Static_32bit-Release\RoguePlugin\qmldir ..\..\bin\staging\RoguePlugin\
+xcopy ..\..\bin\build-RetroRogueCollection-Static_32bit-Release\RetroRogueCollection.exe ..\..\bin\staging\
+xcopy ..\..\bin\build-RetroRogueCollection-Static_32bit-Release\RoguePlugin\qmldir ..\..\bin\staging\RoguePlugin\ /F /R /Y /I
+xcopy ..\..\bin\Win32\Release\Rogue_*.dll ..\..\bin\staging\
+xcopy ..\..\bin\Win32\Release\Rogomatic*.dll ..\..\bin\staging\
+xcopy ..\..\rogue.opt ..\..\bin\staging\
+xcopy ..\..\docs\readme.md ..\..\bin\staging\
+xcopy ..\..\readme.md ..\..\bin\staging\rlog\
