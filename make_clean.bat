c6b089c0e1e7ebaf621d42bcf280af024dc85430
Mike Kaminski <mikeyk730@gmail.com>
2019-10-25 09:22:24 -0700

    Don't blindly git clean to minimize risk of mistakes

--- make_clean.bat
+++ make_clean.bat
@@ -1,1 +1,8 @@
-git clean -xdf -e src/RogueCollectionQml/RetroRogueCollection.pro.user -e src/RogueCollectionQml/RogueCollection.pro.user -e src/RogueCollectionSdl/RogueCollection.vcxproj.user -e Rogue.VC.db -e Rogue.VC.VC.opendb
+@echo off
+echo Performing dry run:
+echo -------------------
+git clean -xdfn -e src/RogueCollectionQml/RetroRogueCollection.pro.user -e src/RogueCollectionQml/RogueCollection.pro.user -e src/RogueCollectionSdl/RogueCollection.vcxproj.user -e Rogue.VC.db -e Rogue.VC.VC.opendb
+echo.
+echo Command to remove files listed above:
+echo -------------------------------------
+echo git clean -xdf -e src/RogueCollectionQml/RetroRogueCollection.pro.user -e src/RogueCollectionQml/RogueCollection.pro.user -e src/RogueCollectionSdl/RogueCollection.vcxproj.user -e Rogue.VC.db -e Rogue.VC.VC.opendb
