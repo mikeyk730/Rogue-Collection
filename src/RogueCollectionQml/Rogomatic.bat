a4ad18342c19d1ab9e3c45508da38af93f4b97d2
mikeyk730 <mikeyk730@gmail.com>
2025-08-24 01:10:24 -0400

    Update rogomatic .bat files (#39)

--- src/RogueCollectionQml/Rogomatic.bat
+++ src/RogueCollectionQml/Rogomatic.bat
@@ -1,1 +1,12 @@
-RogueCollection.exe --rogomatic
+@echo Run Rogomatic for which game?
+@echo.
+@echo a) PC Rogue 1.48
+@echo b) PC Rogue 1.1
+@echo c) Unix Rogue 5.4.2
+@echo d) Unix Rogue 5.3
+@echo e) Unix Rogue 5.2.1
+@echo f) Unix Rogue 3.6.3
+@echo.
+@set /p "id=Enter letter: "
+
+RogueCollection.exe --rogomatic %id%
