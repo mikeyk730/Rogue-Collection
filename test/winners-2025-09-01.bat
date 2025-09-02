0f0d3ed6ffb16c3201e88d78eb8b3b2753f17a14
unknown <mikeyk730@gmail.com>
2025-09-01 22:50:49 -0400

    more savs

--- test/winners-2025-09-01.bat
+++ test/winners-2025-09-01.bat
@@ -10,7 +10,7 @@ RogueCollection.exe f --rogomatic --seed 1756585165 --genes "37 60 22 94 86 56 7
 @rem broken, working in 4d7305e31e80c684c9fea1dc28a4124e6c7b6998: RogueCollection.exe e --rogomatic --seed 1756491774 --genes "35 91 96 22 29 13 90 3"
 RogueCollection.exe e --rogomatic --seed 1756552402 --genes "28 91 40 22 29 11 79 3"
 RogueCollection.exe e --rogomatic --seed 1756563590 --genes "44 82 40 22 29 11 95 3"
-@rem broken 2025-09-01: RogueCollection.exe e --rogomatic --seed 1756623415 --genes "24 91 35 22 29 9 79 15"
+@rem broken: RogueCollection.exe e --rogomatic --seed 1756623415 --genes "24 91 35 22 29 9 79 15"
 
 @rem 5.3 amulet
 RogueCollection.exe d --rogomatic --seed 1756407616 --genes "12 64 82 56 12 64 77 81"
